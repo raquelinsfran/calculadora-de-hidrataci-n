ee1dc8e0fb8ee1c69bcebb7f0f4fdb96ac24de26
Raquel <115370773+raquelinsfran@users.noreply.github.com>
2023-04-24 22:04:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
+
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    //validamos que se cargue un dato:
+    if (DATO > 0){
+        ERROR.style.display = 'none'
+        let flujo = calcFlujo(DATO);
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = 'Volumen Diario: ' + flujo + ' cc';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/dia';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } else {
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    }
+})
+function calcFlujo(peso){
+    let resto = peso;
+    let flujo = 0;
+    if (resto>20){
+        let aux = resto-20;
+        flujo += aux*20;
+        resto -= aux;
+    } 
+    if (resto>10){
+        let aux = resto-10;
+        flujo += aux*50;
+        resto -= aux;
+    }
+    flujo += resto*100;
+    return flujo;
+}
